66d53a356531af2938f387b6eb0a003099cccdcb
curt evans <125291626+jcmed@users.noreply.github.com>
2024-07-24 21:24:10 -0400

    new

--- inspection/urls.py
+++ inspection/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name="home"),
-    
-  
+    path('fetch_api_data/',views.fetch_api_data, name='fetch_api_data'),
+    path('active/',views.active, name= "active"),
+    path('911/',views.active_2, name="911"),
+    path('home2', views.home2, name="home2"),
+    path('home3', views.home3, name="home3"),
     
 ]
-#Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNTQwYzcwNTY5ZjNjNmQ3MzNjYjk4YmU1IiwiYXBwbGljYXRpb25faWQiOiI1M2ViYThlOWFjOWMxM2ExMGNiZGQwMzUiLCJpYXQiOjE2ODQxOTgyNzV9.KUNorUvgReXlu-Zy2aAE587f7S9Iv4aGy8aCzaE5HD4
--- inspection/views.py
+++ inspection/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-
+from django.http import JsonResponse
+import requests
 
 
 import os
@@ -9,8 +10,37 @@ import os
 def home(request):
 	return render(request, 'home.html', {})
 
+def home2(request):
+	return render(request, 'home2.html', {})
 
 
+
+def home3(request):
+	return render(request, 'home3.html', {})
+
+def active_2(request):
+	return render(request, '911.html', {})
+
+def fetch_api_data(request):
+    access_token = "913c16230cfb43125cbb7098dd3a9ab9a1e693a1"
+    base_api_uri = 'https://access.active911.com/interface/open_api/api/'
+
+    context = {
+          'access_token': access_token,
+	}
+    
+    return render(request, 'fetch_api_data.html', context)
+
+
+def active(request):
+    access_token = "7c56462989ad2fd79eebdcf171fbb32142da9f8"
+    base_api_uri = 'https://access.active911.com/interface/open_api/api/'
+
+    #context = {
+          #'access_token': access_token,
+	#}
+    #return render(request, 'active.html', context)
+    return JsonResponse({'base_api_uri' : agency})
 
 
 
